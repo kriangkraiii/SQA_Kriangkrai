65af221f8416586142076ec555dd4d95fedd7e5a
kriangkraiii <kriangkrai.p@kkumail.com>
2025-07-22 13:30:12 +0700

    update

---
+++ Assignment/Lab6_DT/TVPackage/src/sqa/main/TVPlanTest2.java
@@ -0,0 +1,127 @@
+/**
+ * 
+ */
+package sqa.main;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+class TVPlanTest2 {
+
+	/**
+	 * Test method for {@link sqa.main.TVPlan#TVPlan(boolean, boolean, boolean)}.
+	 */
+	@Test
+	final void testTVPlan() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link sqa.main.TVPlan#pricePerMonth(sqa.main.TVPlan.TVPackage)}.
+	 */
+	@Test
+	final void testPricePerMonth() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
